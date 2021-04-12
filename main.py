b962623bb9d5cf0b48795520bb930c772f2b0c05
yanwei <13026132677@163.com>
2021-04-12 17:48:48 +0800

    测试

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-from flask import Flask
+import json
+from flask import Flask,jsonify,request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 @app.route("/test")
 def test():
-    return "this just a test for uni-app with flask on cloud"
+    return jsonify({"text":"this just a test for uni-app with flask on cloud"}) 
 
 @app.route("/hi")
 def say_hi():
     return "hi"
-app.run()
+@app.route('/upload')
+def upload_res():
+    pass
+app.run(host="0.0.0.0")
